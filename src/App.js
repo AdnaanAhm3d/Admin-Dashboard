fab5d9c70e901a87c089e20a0b541a9e332ab3e7
AdnaanGit <adnaana06@gmail.com>
2022-11-25 02:39:13 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { ColorModeContext, useMode } from './theme'
 import { CssBaseLine, ThemeProvide } from '@mui/material'
 
 function App() {
-  return <div className='app'></div>
+  return (
+    <div className='app'>
+      <h2>helloo</h2>
+    </div>
+  )
 }
 
 export default App

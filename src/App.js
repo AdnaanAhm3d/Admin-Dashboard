682e2a6f594f407a1831c532c390184c8ac05e12
AdnaanGit <adnaana06@gmail.com>
2022-11-30 19:55:36 +0530

    calendernfaq commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Topbar from './scenes/global/Topbar'
 import Sidebar from './scenes/global/Sidebar'
 import Dashboard from './scenes/dashboard'
 import { ProSidebarProvider } from 'react-pro-sidebar'
-// import Invoices from './scenes/global/iidebar'
-// import Contacts from './scenes/global/contacts'
+import Invoices from './scenes/invoices/index'
+import Contacts from './scenes/contacts/index'
+import Team from './scenes/team/index'
+import Form from './scenes/form/index'
+import Calender from './scenes/calender/index'
+import FAQ from './scenes/faq/index'
 // import Bar from './scenes/global/bar'
-// import Form from './scenes/global/form'
 // import Line from './scenes/global/line'
 // import Pie from './scenes/global/pie'
-// import FAQ from './scenes/global/faq'
 // import Geography from './scenes/global/geography'
-// import Calender from './scenes/global/calender'
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -30,16 +31,16 @@ function App() {
             <Topbar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
-              {/* <Route path='/team' element={<Team />} /> */}
-              {/* <Route path='/contacts' element={<Contacts />} /> */}
-              {/* <Route path='/invoices' element={<Invoices />} /> */}
-              {/* <Route path='/form' element={<Form />} /> */}
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/calendar' element={<Calender />} />
+              <Route path='/faq' element={<FAQ />} />
               {/* <Route path='/bar' element={<Bar />} /> */}
               {/* <Route path='/pie' element={<Pie />} /> */}
               {/* <Route path='/line' element={<Line />} /> */}
-              {/* <Route path='/faq' element={<FAQ />} /> */}
               {/* <Route path='/geography' element={<Geography />} /> */}
-              {/* <Route path='/calender' element={<Calender />} /> */}
             </Routes>
           </main>
         </div>

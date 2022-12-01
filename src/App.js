72292df5d8d25f64c519aa7eae99893548469143
AdnaanGit <adnaana06@gmail.com>
2022-12-02 03:50:16 +0530

    linechart

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import Form from './scenes/form/index'
 import Calender from './scenes/calender/index'
 import FAQ from './scenes/faq/index'
 import Bar from './scenes/bar/index'
-// import Line from './scenes/global/line'
-// import Pie from './scenes/global/pie'
+import Pie from './scenes/pie/index'
+import Line from './scenes/linechart/index'
 // import Geography from './scenes/global/geography'
 
 function App() {
@@ -38,8 +38,8 @@ function App() {
               <Route path='/calendar' element={<Calender />} />
               <Route path='/faq' element={<FAQ />} />
               <Route path='/bar' element={<Bar />} />
-              {/* <Route path='/pie' element={<Pie />} /> */}
-              {/* <Route path='/line' element={<Line />} /> */}
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
               {/* <Route path='/geography' element={<Geography />} /> */}
             </Routes>
           </main>
